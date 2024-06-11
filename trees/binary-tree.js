4e8a6deb5ea21fa4efc1fd5e29fe2e888c2420f2
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-12 00:11:02 +0530

    Added program for binary tree

---
+++ trees/binary-tree.js
@@ -0,0 +1,57 @@
+// BINARY TREE Implementation
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return;
+        }
+
+        let current = this.root;
+        while(true) {
+            if (value < current.value) {
+                if (!current.left) {
+                    current.left = newNode;
+                    return;
+                }
+                current = current.left;
+            } else {
+                if (!current.right) {
+                    current.right = newNode;
+                    return;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    inOrderTraversal(node, result = []) {
+        if (node) {
+            this.inOrderTraversal(node.left, result);
+            result.push(node.value);
+            this.inOrderTraversal(node.right, result);
+        }
+        return result;
+    }
+}
+
+const tree = new BinaryTree();
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+tree.insert(4);
+tree.insert(5);
+console.log(tree.inOrderTraversal(tree.root)); // [1, 2, 3, 4, 5]
